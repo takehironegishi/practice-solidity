26f7069af67322b38a86792441802e58649b4962
takehironegishi <takehiro.n61@gmail.com>
2021-07-29 22:31:57 +0900

    feat: implement mock index.js

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,7 +1,16 @@
 import Web3 from 'web3';
+import { RINKEBY_URL } from '../config';
 
-window.ethereum.request({ method: 'eth_requestAccounts' });
+let web3;
 
-const web3 = new Web3(window.ethereum);
+if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
+  // We are in the browser and metamask is running.
+  window.ethereum.request({ method: 'eth_requestAccounts' });
+  web3 = new Web3(window.ethereum);
+} else {
+  // We are on the server or the user is not running metamask
+  const provider = new Web3.providers.HttpProvider(RINKEBY_URL);
+  web3 = new Web3(provider);
+}
 
 export default web3;
