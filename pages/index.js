78f360f75392a85510b717159d4a1dc974990912
takehironegishi <takehiro.n61@gmail.com>
2021-07-30 15:29:25 +0900

    feat: implement card and button in index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,48 @@
 import factory from '../ethereum/factory';
+import {
+  Button,
+  Card,
+  CardContent,
+  Link,
+  Typography,
+  makeStyles,
+} from '@material-ui/core';
+import { AddCircle } from '@material-ui/icons';
+
+const useStyle = makeStyles({
+  button: {
+    padding: '8px 16px',
+    textTransform: 'none',
+  },
+});
 
 const CampaignIndex = ({ campaigns }) => {
-  return <div>{campaigns[0]}</div>;
+  const classes = useStyle();
+
+  return (
+    <div>
+      <h3>Open Campaigns</h3>
+      {campaigns.map((address) => (
+        <Card>
+          <CardContent>
+            <Typography variant="h6">{address}</Typography>
+            <Link component="button" variant="body1">
+              View Campaign
+            </Link>
+          </CardContent>
+        </Card>
+      ))}
+      <Button
+        variant="contained"
+        color="primary"
+        startIcon={<AddCircle />}
+        className={classes.button}
+        size="large"
+      >
+        Create Campaign
+      </Button>
+    </div>
+  );
 };
 
 export const getServerSideProps = async () => {
