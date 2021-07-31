0f88721860a3ffc7938c24fc2e29c7a8688142d6
takehironegishi <takehiro.n61@gmail.com>
2021-07-31 12:26:56 +0900

    feat: create campaign show page

---
+++ pages/campaigns/[contract_address].js
@@ -0,0 +1,11 @@
+import { Layout } from '../../components/Layout';
+
+const CampaignShow = () => {
+  return (
+    <Layout>
+      <h3>Campaign Show</h3>
+    </Layout>
+  );
+};
+
+export default CampaignShow;
